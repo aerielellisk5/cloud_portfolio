b0da3be0686d79b82727a388034a7ed0aad47991
Aeriel <aerielellisk5@gmail.com>
2023-12-15 21:21:24 -0800

    trying to figure out lambda

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+let counter = 0
 let project_one = document.querySelector('#project_one_likes')
 let project_one_like_count = 0
 project_one.addEventListener("click", increaseCount)
 let project_one_like_count_html = document.querySelector("#project_one_like_count")
+
 
 function increaseCount() {
     project_one_like_count = project_one_like_count +1
@@ -10,4 +12,11 @@ function increaseCount() {
 }
 
 // checkout the onload for the document
+
+
+function onloadcounter () {
+    counter = counter + 1
+    console.log(counter)
+    return counter
+}
 
