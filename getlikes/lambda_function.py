7356a9e4d6c34d363f30de0c459f4573f3103c54
Aeriel <aerielellisk5@gmail.com>
2023-12-29 19:45:18 -0800

    adding some linting to my lambda function

--- getlikes/lambda_function.py
+++ getlikes/lambda_function.py
@@ -1,49 +1,34 @@
-import psycopg2
+# pylint: disable=no-value-for-parameter
+
+"""
+Added for pylint
+can go to .pylintrc to change stuff for linting
+"""
+
 import logging
-import sys
 import os
+import sys
+import psycopg2
 
-
-try:    
+try:
     user_name = os.environ['USER_NAME']
     password = os.environ['PASSWORD']
     rds_host = os.environ['RDS_HOST']
     rds_port = os.environ['RDS_PORT']
     db_name = os.environ['DB_NAME']
-    
-    conn = psycopg2.connect(host=rds_host, user=user_name, password=password, dbname=db_name, port=rds_port)
+
 except psycopg2.Error as e:
     logging.Logger.error("ERROR: Unexpected error: Could not connect to Postgres instance.")
     logging.Logger.error(e)
     sys.exit()
-    
-    
-# logging.Logger.info("SUCCESS: Connection to the RDS Postgres instance succeeded")
-    
-# def lambda_handler(event, context):
-#     with conn.cursor() as cur:
-#         cur.execute(
-#                 """
-#                 SELECT counter from counter
-#                 ORDER BY id DESC
-#                 LIMIT 1;
-#                 """
-#             )
-#         data = cur.fetchone()
-#         # print(data)
-        
-
-        
-#         conn.commit()
-#         cur.close()
-#         conn.close()
-    
-#         return data
-
-
-
 
 def lambda_handler(event, context):
+    """
+    Added for pylint
+    """
+    print(event)
+    print(context)
+    conn = psycopg2.connect(host=rds_host, user=user_name, password=password, dbname=db_name, port=rds_port)
     with conn.cursor() as cur:
         cur.execute(
                 """
@@ -53,15 +38,7 @@ def lambda_handler(event, context):
                 """
             )
         data = cur.fetchone()
-        # print(data)
-        
-
-        
         conn.commit()
         cur.close()
         conn.close()
-    
         return data
-    
-        
-        
--- script.js
+++ script.js
@@ -1,14 +1,79 @@
-let counter = 0
+// let counter = 0
+// let project_one = document.querySelector('#project_one_likes')
+// let project_one_like_count = 0
+// project_one.addEventListener("click", increaseCount)
+// let project_one_like_count_html = document.querySelector("#project_one_like_count")
+
+
+// function increaseCount() {
+//     project_one_like_count = project_one_like_count +1
+//     project_one_like_count_html.innerHTML = project_one_like_count
+//     return project_one_like_count
+// }
+
+
+
 let project_one = document.querySelector('#project_one_likes')
-let project_one_like_count = 0
-project_one.addEventListener("click", increaseCount)
 let project_one_like_count_html = document.querySelector("#project_one_like_count")
 
 
-function increaseCount() {
-    project_one_like_count = project_one_like_count +1
-    project_one_like_count_html.innerHTML = project_one_like_count
-    return project_one_like_count
+
+project_one.addEventListener("click", async function(){
+    try {
+        const response = await incrementCountInDatabase();
+        console.log('Response from Gateway:', response);
+        project_one_like_count_html.innerHTML = response
+
+
+
+    }
+    catch (error) {
+        console.error('Error incrementing count:', error);
+    }
+})
+
+async function incrementCountInDatabase() {
+    try {
+
+        let count = project_one_like_count_html.innerHTML
+        const response = await fetch('https://4s03q312d3.execute-api.us-east-1.amazonaws.com/increase_likes', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: '{}',
+            // body: '{pageid:5}, can interpret that as the actual id to send to the database
+            // mode: 'no-cors',
+            // credentials: 'include',
+        })
+        // const value = await response.json()
+        console.log(response.status)
+        return await response.text()
+
+    }
+
+    catch (error) {
+        throw new Error(`Failed to increment count: ${error.message}`);
+    }
 }
 
 
+async function getCount() {
+    try {
+        const response = await fetch('https://4s03q312d3.execute-api.us-east-1.amazonaws.com/projects');
+        console.log('was able to connect to the api layer')
+        console.log('attempting to get some json back')
+        const data = await response.json();
+        // project_count = str(data[0])
+
+        project_one_like_count_html.innerHTML = data
+    }
+
+    catch (error) {
+        console.error('Error fetching data:', error)
+    }
+
+    
+}
+
+window.onload = getCount();
